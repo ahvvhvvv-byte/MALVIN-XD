8381e90043df53f890db74601ca7b89cc0382f9b
ahvvhvvv-byte <ahvvhvvv@gmail.com>
2025-08-04 11:47:06 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~931EAbxC#Hl0o0fOegv1tAsxpkkxuEa4AVs8KsaVvpv0rn-anxmY ",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "201096593735", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
